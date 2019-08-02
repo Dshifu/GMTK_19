61b06fa59825eaf7b5707095b7a75ab622235cd8
AleksandrBerestovoy <aleksandrberestovoy@gmail.com>
2019-08-03 02:49:20 +0300

    add character movement controll

---
+++ src/GMTK_19/Assets/CharacterControllerSceneAndAssets/CharacterMovementController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class CharacterMovementController : MonoBehaviour
+{
+    public Rigidbody2D rigidbody2D;
+    public int characterVerticalSpeedMultiplier = 5;
+    public int characterRotationSpeedMultiplier = 100;
+
+    private void Awake()
+    {
+        rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        var move = Input.GetAxis("Vertical");
+        var rotate = Input.GetAxis("Horizontal");
+
+        rigidbody2D.AddTorque(rotate * characterRotationSpeedMultiplier * -1f);
+        rigidbody2D.AddForce((Vector2)transform.up * characterVerticalSpeedMultiplier * move);
+    }
+}
