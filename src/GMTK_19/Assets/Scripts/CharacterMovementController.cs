7367569c4f842527b9e1389933dc6aa5ffbfe3aa
Yurii_Laukhin <yurii.laukhin@binibambini.com>
2019-08-03 11:18:31 +0300

    Refactor Move System

--- src/GMTK_19/Assets/Scripts/CharacterMovementController.cs
+++ src/GMTK_19/Assets/Scripts/CharacterMovementController.cs
@@ -9,9 +9,9 @@ public class CharacterMovementController : MonoBehaviour
     public int characterVerticalSpeedMultiplier = 500;
     public int characterRotationSpeedMultiplier = 1500;
     public Rigidbody2D rigidbodyComponent;
-    private MovementBonusSettings movementBonusSettings = new MovementBonusSettings();
 
-    private bool isMoveBonusActive;
+    public float speedBonusMultiply = 1f;
+    public bool isMovementBonus = false;
 
     private void Awake()
     {
@@ -25,52 +25,13 @@ public class CharacterMovementController : MonoBehaviour
 
         rigidbodyComponent.AddTorque(rotate * characterRotationSpeedMultiplier * -1f);
 
-        if (!isMoveBonusActive)
+        if (!isMovementBonus)
         {
-            rigidbodyComponent.AddForce((Vector2)transform.up * characterVerticalSpeedMultiplier * move);
+            rigidbodyComponent.AddForce(characterVerticalSpeedMultiplier * move * (Vector2)transform.up);
             return;
         }
-
-        if(Input.GetAxisRaw("Vertical") < 1f)
-            return;
-
-        switch (movementBonusSettings.movementBonusType)
-        {
-            case MovementBonusSettings.MovementBonusType.AK:
-            case MovementBonusSettings.MovementBonusType.BEANS:
-            case MovementBonusSettings.MovementBonusType.FIRESHIT:
-                rigidbodyComponent.AddForce((Vector2)transform.up * characterVerticalSpeedMultiplier * movementBonusSettings.straightSpeedMultiplier);
-                movementBonusSettings.effectiveTime -= Time.deltaTime;
-                break;
-            case MovementBonusSettings.MovementBonusType.SHAMPOO:
-            case MovementBonusSettings.MovementBonusType.COKE:
-                StartCoroutine(AccelerateForTimeAfterDelay((Vector2)transform.up * characterVerticalSpeedMultiplier * movementBonusSettings.straightSpeedMultiplier,
-                                                                movementBonusSettings.movementBonusType));
-                break;
-        }
-
-        if (movementBonusSettings.effectiveTime < 0)
-            isMoveBonusActive = false;
-
-    }
-
-    private IEnumerator AccelerateForTimeAfterDelay(Vector2 force, MovementBonusSettings.MovementBonusType movementBonusType)
-    {
-        yield return new WaitForSeconds(movementBonusSettings.delayBeforeActivation);
-        if(movementBonusSettings.movementBonusType != movementBonusType)
-            yield break;
-        while (movementBonusSettings.effectiveTime > 0)
-        {
-            rigidbodyComponent.AddForce(force);
-            movementBonusSettings.effectiveTime -= Time.deltaTime;
-            yield return null;
-        }
-    }
-
-    public void SetMovementBonus(MovementBonusSettings collectedMovementBonusSettings)
-    {
-        movementBonusSettings = collectedMovementBonusSettings;
-        isMoveBonusActive = true;
+        
+        rigidbodyComponent.AddForce(characterVerticalSpeedMultiplier * speedBonusMultiply * (Vector2)transform.up);
     }
 
     [System.Serializable]
--- src/GMTK_19/Assets/Scripts/MovementBonusEffectController.cs
+++ src/GMTK_19/Assets/Scripts/MovementBonusEffectController.cs
@@ -1,13 +1,68 @@
-﻿using UnityEngine;
+﻿using System;
+using System.Collections;
+using UnityEngine;
 
 [RequireComponent(typeof(Collider2D), typeof(Rigidbody2D), typeof(CharacterMovementController))]
 public class MovementBonusEffectController : MonoBehaviour
 {
+    private CharacterMovementController characterMovementController;
+    
+    private CharacterMovementController.MovementBonusSettings movementBonusSettings = null;
+
+    private void Start()
+    {
+        characterMovementController = GetComponent<CharacterMovementController>();
+    }
+
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (!other.transform.CompareTag(PrefsName.MovementBonus)) return;
         var bonusEffect = other.GetComponent<MovementBonusEffect>();
-        transform.GetComponent<CharacterMovementController>().SetMovementBonus(bonusEffect.movementBonusSettings);
+        movementBonusSettings = bonusEffect.movementBonusSettings;
         bonusEffect.CollectMovementBonus();
+    }
+
+    private void FixedUpdate()
+    {
+        if(movementBonusSettings == null)
+            return;
+        if (movementBonusSettings.effectiveTime < 0)
+        {
+            characterMovementController.isMovementBonus = false;
+            characterMovementController.speedBonusMultiply = 1f;
+        }
+        
+        if(Input.GetAxisRaw("Vertical") < 1f)
+            return;
+
+        switch (movementBonusSettings.movementBonusType)
+        {
+            case CharacterMovementController.MovementBonusSettings.MovementBonusType.AK:
+            case CharacterMovementController.MovementBonusSettings.MovementBonusType.BEANS:
+            case CharacterMovementController.MovementBonusSettings.MovementBonusType.FIRESHIT:
+                characterMovementController.speedBonusMultiply = movementBonusSettings.straightSpeedMultiplier;
+                movementBonusSettings.effectiveTime -= Time.deltaTime;
+                break;
+            case CharacterMovementController.MovementBonusSettings.MovementBonusType.SHAMPOO:
+            case CharacterMovementController.MovementBonusSettings.MovementBonusType.COKE:
+                
+                StartCoroutine(AccelerateForTimeAfterDelay(movementBonusSettings.straightSpeedMultiplier,
+                    movementBonusSettings.movementBonusType));
+                break;
+        }
+        characterMovementController.isMovementBonus = true;
+    }
+    
+    private IEnumerator AccelerateForTimeAfterDelay(float speedMultiplier, CharacterMovementController.MovementBonusSettings.MovementBonusType movementBonusType)
+    {
+        yield return new WaitForSeconds(movementBonusSettings.delayBeforeActivation);
+        if(movementBonusSettings.movementBonusType != movementBonusType)
+            yield break;
+        while (movementBonusSettings.effectiveTime > 0)
+        {
+            characterMovementController.speedBonusMultiply = movementBonusSettings.straightSpeedMultiplier;
+            movementBonusSettings.effectiveTime -= Time.deltaTime;
+            yield return null;
+        }
     }
 }
