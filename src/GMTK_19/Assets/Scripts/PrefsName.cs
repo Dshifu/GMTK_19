33bed786606592905d14bac1bcf38aeb0ed64f41
AleksandrBerestovoy <aleksandrberestovoy@gmail.com>
2019-08-03 13:19:59 +0300

    add animator and setup particlPlay, fix particles

--- src/GMTK_19/Assets/Scripts/PrefsName.cs
+++ src/GMTK_19/Assets/Scripts/PrefsName.cs
@@ -7,4 +7,15 @@ public class PrefsName : MonoBehaviour
     public const string ReducerPanic = "ReducerPanic";
     public const string VelocityPanic = "VelocityPanic";
     public const string MoveTrigger = "Move";
+
+
+    public class AnimatorState
+    {
+        public const string FireShit = "FireShit";
+        public const string Goroh = "Goroh";
+        public const string Coka = "Coka";
+        public const string Shampoo = "Shampoo";
+        public const string AK = "AK";
+        public const string IsNeedToPlayParticle = "IsNeedToPlayParticle";
+    }
 }
