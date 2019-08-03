895240602b4cfcbb0991a461bff536a94e1ea355
AleksandrBerestovoy <aleksandrberestovoy@gmail.com>
2019-08-03 04:57:31 +0300

    create levelGenerator

--- src/GMTK_19/Assets/CharacterControllerSceneAndAssets/CharacterMovementController.cs
+++ src/GMTK_19/Assets/CharacterControllerSceneAndAssets/CharacterMovementController.cs
@@ -2,15 +2,9 @@
 
 public class CharacterMovementController : MonoBehaviour
 {
-<<<<<<< HEAD
-    public Rigidbody2D rigidbody2D;
     public int characterVerticalSpeedMultiplier = 500;
     public int characterRotationSpeedMultiplier = 1500;
-=======
     public Rigidbody2D rigidbodyComponent;
-    public int characterVerticalSpeedMultiplier = 5;
-    public int characterRotationSpeedMultiplier = 100;
->>>>>>> 5aad7ecf7e8ee1f1d621687e353006725510cd3d
 
     private void Awake()
     {
---
+++ src/GMTK_19/Assets/LevelGenerator.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using Sirenix.OdinInspector;
+using UnityEngine;
+
+public class LevelGenerator : MonoBehaviour
+{
+    [Required] public GameObject[] environmentElements;
+    [Required] public GameObject[] environmentElementsPositions;
+    [Required] public GameObject[] secondCharacterPotentialPositions;
+    [Required] public GameObject secondCharacter;
+
+    private void Awake()
+    {
+        GenerateCurrentLevel();
+
+        DestroyArrayElements(environmentElementsPositions);
+        DestroyArrayElements(secondCharacterPotentialPositions);
+    }
+
+    private void GenerateCurrentLevel()
+    {
+        var envElementsCount = environmentElements.Length;
+        var currentElementIndex = 0;
+        var parentForBoosts = new GameObject("BoostsParent");
+
+        parentForBoosts.transform.SetParent(transform);
+
+        for (var elementPosition = 0; elementPosition < environmentElementsPositions.Length; elementPosition++)
+        {
+            Instantiate(environmentElements[currentElementIndex], environmentElementsPositions[elementPosition].transform.position,
+                Quaternion.identity).transform.SetParent(parentForBoosts.transform);
+
+            currentElementIndex++;
+            if (currentElementIndex >= envElementsCount)
+                currentElementIndex = 0;
+        }
+
+        var rand = Random.Range(0, secondCharacterPotentialPositions.Length);
+        Instantiate(secondCharacter, secondCharacterPotentialPositions[rand].transform.position, Quaternion.identity).transform.SetParent(parentForBoosts.transform);
+    }
+
+    private void DestroyArrayElements(IEnumerable<GameObject> arr)
+    {
+        foreach (var element in arr)
+            Destroy(element);
+    }
+}
