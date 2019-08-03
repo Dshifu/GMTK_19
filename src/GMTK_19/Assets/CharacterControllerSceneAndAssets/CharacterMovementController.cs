5aad7ecf7e8ee1f1d621687e353006725510cd3d
Yurii_Laukhin <yurii.laukhin@binibambini.com>
2019-08-03 03:30:43 +0300

    Fix Warning

--- src/GMTK_19/Assets/CharacterControllerSceneAndAssets/CharacterMovementController.cs
+++ src/GMTK_19/Assets/CharacterControllerSceneAndAssets/CharacterMovementController.cs
@@ -2,13 +2,13 @@
 
 public class CharacterMovementController : MonoBehaviour
 {
-    public Rigidbody2D rigidbody2D;
+    public Rigidbody2D rigidbodyComponent;
     public int characterVerticalSpeedMultiplier = 5;
     public int characterRotationSpeedMultiplier = 100;
 
     private void Awake()
     {
-        rigidbody2D = GetComponent<Rigidbody2D>();
+        rigidbodyComponent = GetComponent<Rigidbody2D>();
     }
 
     private void FixedUpdate()
@@ -16,7 +16,7 @@ public class CharacterMovementController : MonoBehaviour
         var move = Input.GetAxis("Vertical");
         var rotate = Input.GetAxis("Horizontal");
 
-        rigidbody2D.AddTorque(rotate * characterRotationSpeedMultiplier * -1f);
-        rigidbody2D.AddForce((Vector2)transform.up * characterVerticalSpeedMultiplier * move);
+        rigidbodyComponent.AddTorque(rotate * characterRotationSpeedMultiplier * -1f);
+        rigidbodyComponent.AddForce((Vector2)transform.up * characterVerticalSpeedMultiplier * move);
     }
 }
