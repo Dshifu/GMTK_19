fc9af56516f8641e866ccea270d348cc1527da0f
Yurii_Laukhin <yurii.laukhin@binibambini.com>
2019-08-04 17:43:28 +0300

    Continue to Level Generator

--- src/GMTK_19/Assets/LevelGenerator.cs
+++ src/GMTK_19/Assets/LevelGenerator.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using Sirenix.OdinInspector;
 using UnityEngine;
@@ -11,10 +12,10 @@ public class LevelGenerator : MonoBehaviour
     [Required] public GameObject[] reducers;
     [Required] public GameObject[] movers;
     [Required] public GameObject[] spaceObjects;
+    [Required] public List<GameObject> uniqObjects;
     [Required] public GameObject secondCharacter;
     [Required] public GameObject player;
     [Required] public GameObject baseObjectWithParts;
-    [Required] public GameObject longWayObject;
     [Required] public GameObject[] secondCharacterPotentialPositions;
 
 
@@ -24,77 +25,80 @@ public class LevelGenerator : MonoBehaviour
     private const int MoversCount = 1;
     private const int SpaceCount = 1;
 
-    private void Awake()
+    private IEnumerator Start()
     {
         GenerateCurrentLevel();
+        yield return new WaitForSeconds(0.1f);
+        GenerateCharacters();
 
         //Destroy(baseObjectWithParts);
         DestroyArrayElements(secondCharacterPotentialPositions);
     }
 
-    private void GenerateCurrentLevel()
+    private void GenerateCharacters()
     {
-        var sceneObject1 = new GameObject("Part1");
-        var temp = Instantiate(baseObjectWithParts);
-        GenerateParts(temp, sceneObject1);
-        Destroy(temp);
-        
-        temp = Instantiate(baseObjectWithParts);
-        var sceneObject2 = new GameObject("Part2");
-        int randomScaleX = Random.Range(0, 2) == 0 ? -1 : 1;
-        int randomScaleY = Random.Range(0, 2) == 0 ? -1 : 1;
-        temp.transform.position = new Vector3(40f, 0 + (randomScaleY == -1 ? 20 : 0), 0f);
-        temp.transform.localScale = new Vector3(randomScaleX, randomScaleY, 1f);
-        GenerateParts(temp, sceneObject2);
-        Destroy(temp);
-        
-        temp = Instantiate(baseObjectWithParts);
-        var sceneObject3 = new GameObject("Part3");
-        randomScaleX = Random.Range(0, 2) == 0 ? -1 : 1;
-        randomScaleY = Random.Range(0, 2) == 0 ? -1 : 1;
-        temp.transform.position = new Vector3(0f, 40f + (randomScaleY == -1 ? 20 : 0), 0f);
-        temp.transform.localScale = new Vector3(randomScaleX, randomScaleY, 1f);
-        GenerateParts(temp, sceneObject3);
-        Destroy(temp);
-        
-        temp = Instantiate(baseObjectWithParts);
-        var sceneObject4 = new GameObject("Part4");
-        randomScaleX = Random.Range(0, 2) == 0 ? -1 : 1;
-        randomScaleY = Random.Range(0, 2) == 0 ? -1 : 1;
-        temp.transform.position = new Vector3(40f, 40f + (randomScaleY == -1 ? 20 : 0), 0f);
-        temp.transform.localScale = new Vector3(randomScaleX, randomScaleY, 1f);
-        GenerateParts(temp, sceneObject4);
-        Destroy(temp);
+        var charactersParts = GameObject.FindGameObjectsWithTag(PrefsName.PartForCharacter);
 
-        temp = Instantiate(longWayObject);
-        var sceneObject5 = new GameObject("Part5");
-        GenerateParts(temp, sceneObject5);
-        Destroy(temp);
+        int playerPosIndex = Random.Range(0, charactersParts.Length);
+        int secondCharacterIndex = charactersParts.Length - playerPosIndex;
 
-//        var envElementsCount = environmentElements.Length;
-//        var currentElementIndex = 0;
-//        var parentForBoosts = new GameObject("BoostsParent");
-//
-//        parentForBoosts.transform.SetParent(transform);
-//
-//        for (var elementPosition = 0; elementPosition < environmentElementsPositions.Length; elementPosition++)
-//        {
-//            Instantiate(environmentElements[currentElementIndex], environmentElementsPositions[elementPosition].transform.position,
-//                Quaternion.Euler(0f,0f,Random.Range(0f,360f))).transform.SetParent(parentForBoosts.transform);
-//
-//            currentElementIndex++;
-//            if (currentElementIndex >= envElementsCount)
-//                currentElementIndex = 0;
-//        }
-//
-//        var rand = Random.Range(0, secondCharacterPotentialPositions.Length);
-//        Instantiate(secondCharacter, secondCharacterPotentialPositions[rand].transform.position, Quaternion.identity).transform.SetParent(parentForBoosts.transform);
+        Instantiate(player, charactersParts[playerPosIndex].transform.position, Quaternion.identity);
+        Instantiate(secondCharacter, charactersParts[secondCharacterIndex].transform.position,
+            Quaternion.Euler(0f, 0f, Random.Range(0f, 360f)));
+        
+        for (int i = 0; i < charactersParts.Length; i++)
+        {
+            if(i == playerPosIndex || i== secondCharacterIndex)
+                continue;
+            var position = charactersParts[i].transform.GetChild(Random.Range(0, charactersParts[i].transform.childCount));
+
+            int uniqComponentsNumber = Random.Range(0, uniqObjects.Count);
+            Instantiate(uniqObjects[uniqComponentsNumber], position);
+            uniqObjects.RemoveAt(uniqComponentsNumber);
+        }
+
     }
 
-    private void GenerateParts(GameObject parts, GameObject sceneObject)
+    private void GenerateCurrentLevel()
+    {
+        
+        int count = 0;
+        for (int i = 0; i < 3; i++)
+        for (int j = 0; j < 3; j++)
+            GenerateWord(new Vector3(i%3f * 40f, j%3f * 40f, 0f), count++);
+    }
+
+    private void GenerateWord(Vector3 position, int generatedNumber)
+    {
+        var temp = Instantiate(baseObjectWithParts);
+        var sceneObject = new GameObject("Part" + generatedNumber);
+        var randomScaleX = Random.Range(0, 2) == 0 ? -1 : 1;
+        var randomScaleY = Random.Range(0, 2) == 0 ? -1 : 1;
+        temp.transform.position = new Vector3(position.x, position.y + (randomScaleY == -1 ? 20 : 0), 0f);
+        temp.transform.localScale = new Vector3(randomScaleX, randomScaleY, 1f);
+        GenerateParts(temp, sceneObject, generatedNumber != 5);
+        Destroy(temp);
+    }
+
+    private void GenerateParts(GameObject parts, GameObject sceneObject, bool isCanBeCharacter)
     {
         foreach (Transform part in parts.transform)
         {
+            if (part.CompareTag(PrefsName.PartForCharacter))
+            {
+                if (isCanBeCharacter)
+                {
+                    Instantiate(part, sceneObject.transform, true);
+                    continue;
+                }
+                var position = part.transform.GetChild(Random.Range(0, part.transform.childCount));
+
+                int uniqComponentsNumber = Random.Range(0, uniqObjects.Count);
+                Instantiate(uniqObjects[uniqComponentsNumber], position);
+                uniqObjects.RemoveAt(uniqComponentsNumber);
+                Destroy(part.gameObject);
+            }
+            
             int environmentsSpawned = 0;
             int reducersSpawned = 0;
             int moversSpawned = 0;
