c207c7eab15472f3affd6acfdd462a7e742dfd02
Yurii_Laukhin <yurii.laukhin@binibambini.com>
2019-08-04 15:56:59 +0300

    Level Generator is ok, but not finished

--- src/GMTK_19/Assets/LevelGenerator.cs
+++ src/GMTK_19/Assets/LevelGenerator.cs
@@ -13,12 +13,13 @@ public class LevelGenerator : MonoBehaviour
     [Required] public GameObject[] spaceObjects;
     [Required] public GameObject secondCharacter;
     [Required] public GameObject player;
-    [Required] public GameObject[] partsPositions;
+    [Required] public GameObject baseObjectWithParts;
     [Required] public GameObject[] secondCharacterPotentialPositions;
 
 
     private const int EnvironmentsCount = 2;
-    private const int ReducersCount = 2;
+    private const int BigEnvironmentsCount = 1;
+    private const int ReducersCount = 1;
     private const int MoversCount = 1;
     private const int SpaceCount = 1;
 
@@ -26,62 +27,43 @@ public class LevelGenerator : MonoBehaviour
     {
         GenerateCurrentLevel();
 
-        DestroyArrayElements(partsPositions);
+        //Destroy(baseObjectWithParts);
         DestroyArrayElements(secondCharacterPotentialPositions);
     }
 
     private void GenerateCurrentLevel()
     {
-        var parentForBoosts = new GameObject("SceneObjects");
-        foreach (var part in partsPositions)
-        {
-            int environmentsSpawned = 0;
-            int reducersSpawned = 0;
-            int moversSpawned = 0;
-            int spacesSpawned = 0;
-            int bigEnvironmentsSpawned = 0;
-            foreach (Transform position in part.transform)
-            {
-                if(environmentsSpawned == EnvironmentsCount && reducersSpawned == ReducersCount && moversSpawned == MoversCount && spacesSpawned == SpaceCount)
-                    break;
-                int randomChoose = RandomChoose(environmentsSpawned, moversSpawned, reducersSpawned, spacesSpawned);
-                switch (randomChoose)
-                {
-                    case 0:
-                        if (bigEnvironmentsSpawned == 0)
-                        {
-                            Instantiate(bigEnvironments[Random.Range(0, bigEnvironments.Length)], position.position,
-                                Quaternion.Euler(0f, 0f, Random.Range(0f, 360f))).transform.SetParent(parentForBoosts.transform);
-                            bigEnvironmentsSpawned++;
-                            environmentsSpawned++;
-                        }
-                        else
-                        {
-                            Instantiate(smallEnvironment[Random.Range(0, smallEnvironment.Length)], position.position,
-                                Quaternion.Euler(0f, 0f, Random.Range(0f, 360f))).transform.SetParent(parentForBoosts.transform);
-                            environmentsSpawned++;
-                        }
-                        break;
-                    case 1:
-                        Instantiate(reducers[Random.Range(0, reducers.Length)], position.position,
-                            Quaternion.Euler(0f, 0f, Random.Range(0f, 360f))).transform.SetParent(parentForBoosts.transform);
-                        reducersSpawned++;
-                        break;
-                    case 2:
-                        Instantiate(movers[Random.Range(0, movers.Length)], position.position,
-                            Quaternion.Euler(0f, 0f, Random.Range(0f, 360f))).transform.SetParent(parentForBoosts.transform);
-                        reducersSpawned++;
-                        break;
-                    case 3:
-                        Instantiate(spaceObjects[Random.Range(0, spaceObjects.Length)], position.position,
-                            Quaternion.Euler(0f, 0f, Random.Range(0f, 360f))).transform.SetParent(parentForBoosts.transform);
-                        reducersSpawned++;
-                        break;
-                }
-            }            
-        }
-
+        var sceneObject1 = new GameObject("Part1");
+        var temp = Instantiate(baseObjectWithParts);
+        GenerateParts(temp, sceneObject1);
+        Destroy(temp);
         
+        temp = Instantiate(baseObjectWithParts);
+        var sceneObject2 = new GameObject("Part2");
+        int randomScaleX = Random.Range(0, 2) == 0 ? -1 : 1;
+        int randomScaleY = Random.Range(0, 2) == 0 ? -1 : 1;
+        temp.transform.position = new Vector3(40f, 0 + (randomScaleY == -1 ? 20 : 0), 0f);
+        temp.transform.localScale = new Vector3(randomScaleX, randomScaleY, 1f);
+        GenerateParts(temp, sceneObject2);
+        Destroy(temp);
+        
+        temp = Instantiate(baseObjectWithParts);
+        var sceneObject3 = new GameObject("Part3");
+        randomScaleX = Random.Range(0, 2) == 0 ? -1 : 1;
+        randomScaleY = Random.Range(0, 2) == 0 ? -1 : 1;
+        temp.transform.position = new Vector3(0f, 40f + (randomScaleY == -1 ? 20 : 0), 0f);
+        temp.transform.localScale = new Vector3(randomScaleX, randomScaleY, 1f);
+        GenerateParts(temp, sceneObject3);
+        Destroy(temp);
+        
+        temp = Instantiate(baseObjectWithParts);
+        var sceneObject4 = new GameObject("Part4");
+        randomScaleX = Random.Range(0, 2) == 0 ? -1 : 1;
+        randomScaleY = Random.Range(0, 2) == 0 ? -1 : 1;
+        temp.transform.position = new Vector3(40f, 40f + (randomScaleY == -1 ? 20 : 0), 0f);
+        temp.transform.localScale = new Vector3(randomScaleX, randomScaleY, 1f);
+        GenerateParts(temp, sceneObject4);
+        Destroy(temp);
         
 //        var envElementsCount = environmentElements.Length;
 //        var currentElementIndex = 0;
@@ -102,7 +84,58 @@ public class LevelGenerator : MonoBehaviour
 //        var rand = Random.Range(0, secondCharacterPotentialPositions.Length);
 //        Instantiate(secondCharacter, secondCharacterPotentialPositions[rand].transform.position, Quaternion.identity).transform.SetParent(parentForBoosts.transform);
     }
-    
+
+    private void GenerateParts(GameObject parts, GameObject sceneObject)
+    {
+        foreach (Transform part in parts.transform)
+        {
+            int environmentsSpawned = 0;
+            int reducersSpawned = 0;
+            int moversSpawned = 0;
+            int spacesSpawned = 0;
+            int bigEnvironmentsSpawned = 0;
+            foreach (Transform position in part)
+            {
+                if(environmentsSpawned == EnvironmentsCount && reducersSpawned == ReducersCount && moversSpawned == MoversCount && spacesSpawned == SpaceCount)
+                    break;
+                int randomChoose = RandomChoose(environmentsSpawned, moversSpawned, reducersSpawned, spacesSpawned);
+                switch (randomChoose)
+                {
+                    case 0:
+                        if (bigEnvironmentsSpawned != BigEnvironmentsCount)
+                        {
+                            Instantiate(bigEnvironments[Random.Range(0, bigEnvironments.Length)], position.position,
+                                Quaternion.Euler(0f, 0f, Random.Range(0f, 360f))).transform.SetParent(sceneObject.transform);
+                            bigEnvironmentsSpawned++;
+                            environmentsSpawned++;
+                        }
+                        else
+                        {
+                            Instantiate(smallEnvironment[Random.Range(0, smallEnvironment.Length)], position.position,
+                                Quaternion.Euler(0f, 0f, Random.Range(0f, 360f))).transform.SetParent(sceneObject.transform);
+                            environmentsSpawned++;
+                        }
+                        break;
+                    case 1:
+                        Instantiate(reducers[Random.Range(0, reducers.Length)], position.position,
+                            Quaternion.Euler(0f, 0f, Random.Range(0f, 360f))).transform.SetParent(sceneObject.transform);
+                        reducersSpawned++;
+                        break;
+                    case 2:
+                        Instantiate(movers[Random.Range(0, movers.Length)], position.position,
+                            Quaternion.Euler(0f, 0f, Random.Range(0f, 360f))).transform.SetParent(sceneObject.transform);
+                        reducersSpawned++;
+                        break;
+                    case 3:
+                        Instantiate(spaceObjects[Random.Range(0, spaceObjects.Length)], position.position,
+                            Quaternion.Euler(0f, 0f, Random.Range(0f, 360f))).transform.SetParent(sceneObject.transform);
+                        reducersSpawned++;
+                        break;
+                }
+            }            
+        }
+    }
+
     private int RandomChoose(int environmentsSpawned, int reducersSpawned, int moversSpawned, int spacesSpawned)
     {
         int randomChoose = Random.Range(0,4);
