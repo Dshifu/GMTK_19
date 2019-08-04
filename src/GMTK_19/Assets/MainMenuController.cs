a089c8b136d79f36a9f955c37447ec0fe480a609
AleksandrBerestovoy <aleksandrberestovoy@gmail.com>
2019-08-04 13:10:40 +0300

    add mainMenu Scene

---
+++ src/GMTK_19/Assets/MainMenuController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using Sirenix.OdinInspector;
+using UnityEngine;
+
+public class MainMenuController : MonoBehaviour
+{
+    [Required] [SerializeField] private Animator _mainSceneAnimator;
+
+    private bool _isSceneLoading;
+
+    private IEnumerator Start()
+    {
+        yield return new WaitForSeconds(1f);
+        _mainSceneAnimator.SetBool(PrefsName.AnimatorState.StartOpening, true);
+    }
+
+    public void StartLoadingGameScene()
+    {
+        if (_isSceneLoading) return;
+        _mainSceneAnimator.SetBool(PrefsName.AnimatorState.StartFading, true);
+        _isSceneLoading = true;
+        StartCoroutine(LoadScene());
+    }
+
+    private IEnumerator LoadScene()
+    {
+        yield return new WaitForSeconds(2f);
+        UnityEngine.SceneManagement.SceneManager.LoadScene(1);
+    }
+
+}
